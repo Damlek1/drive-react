dfb3d0de2931f01df46a72743d78849a9f518512
Damlek1 <100087318+Damlek1@users.noreply.github.com>
2022-08-04 12:55:56 +0100

    footer commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Driver from './components/Driver/Driver'
 import Find from './components/Find/Find'
+import Footer from './components/Footer/Footer'
 import Hero from './components/Hero/Hero'
 import Luxury from './components/Luxury/Luxury'
 import Navbar from './components/Navbar/Navbar'
@@ -13,6 +14,7 @@ function App() {
       <Find />
       <Driver />
       <Luxury />
+      <Footer />
     </div>
   );
 }
